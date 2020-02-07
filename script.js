c3a7cc95126d318fe35fad617431bf28093ba3a5
Lysanironman <lysanironman@gmail.com>
2020-02-07 13:13:06 -0500

    Added answers to  Fundamentals Pt1

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hello, World!")
+let num1 = 10;
+let num2 = 50;
+console.log(9 * num1);
+console.log(num1 ** 3);
+console.log(num2 / num1);
+let num3 = 77;
+console.log((4+num1+num2)/num3);
+let a = 10;
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+//You should be getting the hang of this by now… try this sequence
+let max = 57
+let actual = max - 13
+let percentage = actual / max
+console.log(percentage);
